77fde842fb87b4cb230bbf72ae2411c3ecd6ffd9
mocavaMars33 <hgkemail@gmail.com>
2021-05-16 14:58:01 +1000

    waveSpawner

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,31 +1,40 @@
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
     public float speed = 10f;
+
     private Transform target;
-    private int waypointIndex = 0; //move throu each index to get to END
+    private int wavepointIndex = 0; //move throu each index to get to END
 
     void Start()
-    {
-        // use the array created in Waypoint
+    {// use the array created in Waypoint
         target = Waypoints.points[0]; //the first waypoint
-
     }
 
     void Update()
-    {
-        //get the direction vector to move to and move in the direction 
-
+    {//get the direction vector to move to and move in the direction 
 
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
     }
 
+    void GetNextWaypoint()
+    {
+       if(wavepointIndex >= Waypoints.points.Length -1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+
+    
 }

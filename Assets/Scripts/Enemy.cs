c938b2117023fede9e262a6fa1bb9df503243a8b
mocavaMars33 <hgkemail@gmail.com>
2021-05-16 17:39:18 +1000

    navmesh

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -7,34 +8,68 @@ public class Enemy : MonoBehaviour
     private Transform target;
     private int wavepointIndex = 0; //move throu each index to get to END
 
+    public bool isRandomWaypoint = false;
+
+    NavMeshAgent agent;
+
     void Start()
     {// use the array created in Waypoint
+        agent = GetComponent<NavMeshAgent>();
         target = Waypoints.points[0]; //the first waypoint
+        
     }
 
     void Update()
     {//get the direction vector to move to and move in the direction 
 
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        //Vector3 dir = target.position - transform.position;
+        //transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        //if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        //{
+        //    GetNextWaypoint();
+        //}
+        agent.SetDestination(Waypoints.points[wavepointIndex].position);
+
+        float distanceToWaypoint = Vector3.Distance(transform.position,
+                                                    Waypoints.points[wavepointIndex].position);
+
+        if (isRandomWaypoint)
         {
-            GetNextWaypoint();
+            if (distanceToWaypoint < 1f)
+            {
+                wavepointIndex = Random.Range(0, Waypoints.points.Length);
+            }
         }
+        else
+        {
+            if (distanceToWaypoint < 1f)
+            {
+                wavepointIndex++;
+            }
+
+            if (wavepointIndex >= Waypoints.points.Length)
+            {
+                wavepointIndex = 0;
+            }
+        }
+
+
+
+
     }
 
-    void GetNextWaypoint()
-    {
-       if(wavepointIndex >= Waypoints.points.Length -1)
-        {
-            Destroy(gameObject);
-            return;
-        }
+    //void GetNextWaypoint()
+    //{
+    //   if(wavepointIndex >= Waypoints.points.Length -1)
+    //    {
+    //        Destroy(gameObject);
+    //        return;
+    //    }
         
-        wavepointIndex++;
-        target = Waypoints.points[wavepointIndex];
-    }
+    //    wavepointIndex++;
+    //    target = Waypoints.points[wavepointIndex];
+    //}
 
     
 }
